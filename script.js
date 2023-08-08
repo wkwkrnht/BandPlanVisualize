1866306694216c810e5d1aa669f94d5d7a25639c
wkwkrnht <wkwkrnht@github.com>
2023-08-09 03:04:16 +0900

    fix

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ function detectDisplayDirection(){
 
 //CSVファイルを読み込む関数getCSV()の定義
 function getCSV( dir = '', dist = [] ){
-    try{
-        let req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
-        req.open('get', dir, true); // アクセスするファイルを指定
-        req.send(null); // HTTPリクエストの発行
-    }catch(err){
-        console.log(err);
-    }
 
     fetch(dir)
     .then(
@@ -30,7 +23,7 @@ function getCSV( dir = '', dist = [] ){
     )
     .then(
         function(text){
-            console.log(text) ;
+            dist = convertCSVtoArray(text);
         }
     )
     .catch(
@@ -38,11 +31,6 @@ function getCSV( dir = '', dist = [] ){
             console.error("fetch error", err);  //  error処理
         }
     );
-
-    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ
-    req.onload = function(){
-	    dist = convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
-    }
 }
 
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
