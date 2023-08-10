0114960edbc0db3589248f0884b35c984cdf122d
wkwkrnht <wkwkrnht@github.com>
2023-08-10 17:10:18 +0900

    fix

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function getCSV( dir = '' ){
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
 function convertCSVtoArray( text = '' ){ // 読み込んだCSVデータが文字列として渡される
     let result = []; // 最終的な二次元配列を入れるための配列
-    const tmp = str.split('\n'); // 改行を区切り文字として行を要素とした配列を生成
+    const tmp = text.split('\n'); // 改行を区切り文字として行を要素とした配列を生成
 
     // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
     for( let i = 0; i < tmp.length; i++ ){
