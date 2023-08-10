6d6a6bd73ef67761261c5bb448775fb96ba83e52
wkwkrnht <wkwkrnht@github.com>
2023-08-10 18:06:33 +0900

    typo

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function createBandTable(section = '' ){
                 const temp = await getCSV('/BandPlanVisualize/3GPPBandPlan.csv');
                 console.log(temp);
                 return temp;
-            })().valu;
+            })().value;
 
             const ulUpColumun = searchColumunByName(data, 'ULup'),
             ulDownColumun = searchColumunByName(data, 'ULdown'),
