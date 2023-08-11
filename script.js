a5349d5bbbbd7ec07c5cff8d622a342757742982
wkwkrnht <wkwkrnht@github.com>
2023-08-11 19:02:10 +0900

    fix

--- script.js
+++ script.js
@@ -45,76 +45,75 @@ function searchColumunByName( array = [], key = ''){ // From 0 row, searching nu
     return array[0].indexOf(key);
 }
 
-function createBox(parent = '', name = '', up = 0, down = 0){ // Create a box of a air band.
+function createBox(dataset = '', name = '', up = 0, down = 0){ // Create a box of a air band.
     const width = up - down; // This is width set in style.
 
-    parent = document.getElementById(parent); // Search a area to insert a box.
+    parent = document.getElementById('main'); // Search a area to insert a box.
     let box = document.createElement('div'); // Create a element of a box.
     parent.appendChild(box);
 
-    box.setAttribute('class', 'box');
+    box.classList.add('box');
+    box.classList.add('dataset');
     box.setAttribute('data-down', down); // Set a value to note start-point of a air band.
     box.setAttribute('data-width', width); // Set a value to note width of a air band.
     box.innerText = name;
 }
 
 async function createBandTable( section = '' ){ // Create Boxes to each air bands from a dataset.
-    switch(section){ // Detect dataset from input.
-        case '3GPP':
-            const data = await getCSV('/BandPlanVisualize/3GPPBandPlan.csv'), // Loading dataset.
-            // Searching number of columun of each elements
-            ulUpColumun = searchColumunByName(data, 'ULup'),
-            ulDownColumun = searchColumunByName(data, 'ULdown'),
-            dlUpColumun = searchColumunByName(data, 'DLup'),
-            dlDownColumun = searchColumunByName(data, 'DLdown'),
-            nameColumun = searchColumunByName(data, 'Name'),
-            modeColumun = searchColumunByName(data, 'Mode');
+    const
+    // Loading 3GPP dataset.
+    data1 = await getCSV('/BandPlanVisualize/3GPPBandPlan.csv'),
+    // Searching number of columun of each elements
+    ulUpColumun = searchColumunByName(data1, 'ULup'),
+    ulDownColumun = searchColumunByName(data1, 'ULdown'),
+    dlUpColumun = searchColumunByName(data1, 'DLup'),
+    dlDownColumun = searchColumunByName(data1, 'DLdown'),
+    nameColumun = searchColumunByName(data1, 'Name'),
+    modeColumun = searchColumunByName(data1, 'Mode'),
 
-            for( let i = 1; i < data.length; i++ ){
-                let mode = data[i][modeColumun];
-                let name = data[i][nameColumun];
+    // Loading JP dataset.
+    data2 = await getCSV('/BandPlanVisualize/JPBandPlan.csv'),
+    // Searching number of columun of each elements
+    downColumun = searchColumunByName(data2, 'down'),
+    upColumun = searchColumunByName(data2, 'up'),
+    purposeColumun = searchColumunByName(data2, 'Purpose');
 
-                switch(mode){ // Detect a air band contains UL and DL by mode indicator.
-                    case 'FDD':
-                        const nameU = name + '↑';
-                        createBox(section, nameU, data[i][ulUpColumun], data[i][ulDownColumun]);
-                        const nameD = name + '↓';
-                        createBox(section, nameD, data[i][dlUpColumun], data[i][dlDownColumun]);
-                        break;
-                    case 'SUL':
-                        name = name + '↑';
-                        createBox(section, name, data[i][ulUpColumun], data[i][ulDownColumun]);
-                        break;
-                    case 'TDD':
-                        name = name + '↑' + '↓';
-                        createBox(section, name, data[i][dlUpColumun], data[i][dlDownColumun]);
-                        break;
-                    case 'SDL':
-                        name = name + '↓';
-                        createBox(section, name, data[i][dlUpColumun], data[i][dlDownColumun]);
-                        break;
-                    default: // Error handling which is missing mode.
-                        console.log('Mode has not set.');
-                        break;
-                }
-            }
-            break;
-        case 'JP':
-            const data = await getCSV('/BandPlanVisualize/JPBandPlan.csv'), // Loading dataset.
-            // Searching number of columun of each elements
-            downColumun = searchColumunByName(data, 'down'),
-            upColumun = searchColumunByName(data, 'up'),
-            purposeColumun = searchColumunByName(data, 'Purpose');
+    for( let i = 1; i < data1.length; i++ ){
+        let mode = data1[i][modeColumun];
+        let name = data1[i][nameColumun];
 
-            for( let i = 1; i < data.length; i++ ){
-                let mode = data[i][modeColumun];
-                let name = data[i][purposeColumun];
+        switch(mode){ // Detect a air band contains UL and DL by mode indicator.
+            case 'FDD':
+                const nameU = name + '↑';
+                createBox(section, nameU, data1[i][ulUpColumun], data1[i][ulDownColumun]);
+                const nameD = name + '↓';
+                createBox(section, nameD, data1[i][dlUpColumun], data1[i][dlDownColumun]);
+                break;
+            case 'SUL':
+                name = name + '↑';
+                createBox(section, name, data1[i][ulUpColumun], data1[i][ulDownColumun]);
+                break;
+            case 'TDD':
+                name = name + '↑' + '↓';
+                createBox(section, name, data1[i][dlUpColumun], data1[i][dlDownColumun]);
+                break;
+            case 'SDL':
+                name = name + '↓';
+                createBox(section, name, data1[i][dlUpColumun], data1[i][dlDownColumun]);
+                break;
+            default: // Error handling which is missing mode.
+                console.log('Mode has not set.');
+                break;
+        }
+    }
 
-                createBox(section, name, data[i][upColumun], data[i][downColumun]);
-            }
-            break;
-        default:
-            break;
+    const
+
+    for( let i = 1; i < data2.length; i++ ){
+        let mode = data2[i][modeColumun];
+        let name = data2[i][purposeColumun];
+
+        createBox(section, name, data2[i][upColumun], data2[i][downColumun]);
     }
 }
 
